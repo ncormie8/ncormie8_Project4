e7352148fe61339caaffb4cd6e41c77578f2b216
ncormie8 <ncormie8@uwo.ca>
2024-12-09 18:30:26 -0500

    Function creation - Timestamp 2
    - Used values for nspace, ntime, and tau from lab 11 to verify functionality of x_grid, t_grid, and Vx_initial
    - Modified x_grid spacing parameter h to include -L/2 AND L/2 when initializing x_grid
    - Changed Vx_initial to be initialized as empty to better align with given instructions

--- CormierNathan_Project4.py
+++ CormierNathan_Project4.py
@@ -2,40 +2,44 @@ import numpy as np
 from matplotlib import pyplot
 
 # References
-#
-#
+# Lab 11; Reused and adapted:
+#       - x_grid, 
+#       - t_grid, 
+#       - Inital condition code for V(x)
+#       - Method lower casing
+#   
 #
 #
 
 #---------------------------------------------------------------------------------------------------#
 # Project 4
 
-# Initial parameter declarations
-nspace_tbd = 101010101        # number of spatial grid points (int)
-ntime_tbd = 101010101         # number of time steps to be solved over (int)
-tau_tbd = 101010101           # tau is the time step to be solved with (float)
+# Initial parameter declarations (using values from Lab 11 to test functionality for now)
+nspace_tbd = 300         # number of spatial grid points (int)
+ntime_tbd = 501         # number of time steps to be solved over (int)
+tau_tbd = 0.016666666666666666 # tau is the time step to be solved with (float)
 
 # Function that solves the 1D time dependent Schroedinger Equation
 def sch_eqn(nspace, ntime, tau, method='ftcs', length=200, potential=[], wparam=[10,0,0.5]):
     
-    # Converting input arguments into shorter easier versions for calculations
+    # Converting input arguments into shorter better versions for integration
     L = length
-    h = L/nspace      # x_grid spacing parameter
+    h = L/(nspace-1)         # x_grid spacing parameter (minus 1 so it goes perfectly from -L/2 to L/2)
+    method = method.lower()  # modifying input string to be all lowercase for versatility
 
     # Constants
     _plk_given = 1
     _m_given = 0.5
 
-    # Initializing the x grid array to solve over
-    x_grid = np.arange(-L/2,L/2,h)
+    # Initializing the x grid array to solve over (tested - appears to be working properly)
+    x_grid = np.arange(-L/2,L/2+h,h)
 
-    # Initializing the time grid array to solve over
+    # Initializing the time grid array to solve over (tested - appears to be working properly)
     t_grid = np.arange(0,ntime*tau,tau)
 
-    # Initializing the potential array
-    Vx_initial = np.zeros(np.size(x_grid))  # Creating zeros array of potentials to match each x position
+    # Initializing the potential array (tested - appears to be working properly)
+    Vx_initial = np.empty(np.size(x_grid))  # Creating empty array of potentials to match each x position
     Vx_initial[potential] = 1               # Setting V = 1 for all values specified by input arg potential
-
 
     # Logic pathway for FTCS integration (default)
     # if method == 'ftcs':
@@ -57,3 +61,6 @@ def sch_eqn(nspace, ntime, tau, method='ftcs', length=200, potential=[], wparam=
 
     # putting a meaningless return here so i avoid errors while setting up the function
     return t_grid
+
+t_test = sch_eqn(nspace_tbd,ntime_tbd,tau_tbd)
+print(t_test)
