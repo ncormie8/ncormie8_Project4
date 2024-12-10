ce7970249eea26e74beb4b9634db5e2152db2d3e
ncormie8 <ncormie8@uwo.ca>
2024-12-09 20:51:12 -0500

    Function creation - Timestamp 4
    - Updated references
    - Adapted make_initialcond() from lab 10 into make_initialcond_sch() to compute the initial condition of the Gaussian wave packet
    - Copied make_tridiagonal() for later adaptation to be used in evolution matrix construction
    - Moved constants out of sch_eqn() into the global space since they are needed in subfunctions (make_initialcond_sch)
    - Declared constant _imag_i to represent imaginary/complex number i

--- CormierNathan_Project4.py
+++ CormierNathan_Project4.py
@@ -5,21 +5,22 @@ import time                # for code testing and user experience (time.sleep())
 # References
 #  Lab 10; Reused and adapted:
 #       - spectral_radius() to perform stability analysis of FTCS integration 
-#       - Probably going to need the make_initialcond() and make_tridiagonal() for matrix constuction
-
+#       - make_initialcond() for applying initial condition to Shroedingers Equation
+#       - make_tridiagonal() for evolution matrix constuction
 #
 #  Lab 11; Reused and adapted:
-#       - x_grid, 
-#       - t_grid, 
+#       - x_grid definition
+#       - t_grid definition
 #       - Inital condition code for V(x)
-#       - Method lower casing
+#       - Input method lower casing
 #       - Stability analysis logic from spectral_radius() output
 # 
 #  Western Brightspace. (n.d.). Physics 3926, Project 4: The Schrodinger Equation
 #  UWO. https://westernu.brightspace.com/d2l/le/enhancedSequenceViewer/29104?url=https%3A%2F%2F832d4e9b-197e-4ab6-a2ca-c4f7aae74b20.sequences.api.brightspace.com%2F29104%2Factivity%2F2552026%3FfilterOnDatesAndDepth%3D1
 #       - Used provided instuctions to complete Project 4
 #       - Reused and adapted description of sch_eqn() and its parameters for sch_eqn() Docstring
-#
+#  
+#  Numerical Methods For Physics (Python) - Alejandro L. Garcia 2E revised, 2017. pgs 227-240
 #
 
 # Reused functions from previous labs and projects:
@@ -42,13 +43,69 @@ def spectral_radius(A):
     
     return maxAbs
 
-#---------------------------------------------------------------------------------------------------#
+# Lab 10 - Make tridiagonal; used in constructing evolution matrix
+def make_tridiagonal(N, b, d, a):
+    '''Takes input parameters N for size of square array, b for the value along lower/below diagonal,
+    d for the value along the central diagonal, a for the value along the upper/above diagonal. Returns matrix A with
+    lower diagonal values of b, central diagonal values of d, upper diagonal values of a, and all other values of 0,
+    of shape (N,N).'''
+
+    # initializing A array as N x N array of zeros
+    A = np.zeros(shape=(N,N))
+    
+    # filling central diagonal of a with input argument d
+    np.fill_diagonal(a=A,val=d)
+    
+    # initializing BEL array as N x N array with lower diagonal equal to b
+    BEL = np.diag(np.full(shape=(N-1),fill_value=b),k=-1)
+    
+    # initializing ABV array as N x N array with above diagonal equal to a
+    ABV = np.diag(np.full(shape=(N-1),fill_value=a),k=1)
+    
+    # adding upper and lower diagonals to A to be returned
+    A = A + BEL + ABV
+    return A
+
+# Lab 10 - Make initial condition; modified to apply initial condition to Shroedingers Equation
+def make_initialcond_sch(wparam,space_grid):
+    '''Function which computes the initial condition of a Gaussian wave packet. Intakes two input
+    parameters wparam and space_grid. The first argument wparam (list), contains the initial packet width, 
+    initial particle localization, and wave number as shown here; [sig_0, x0, k0]. The second argument 
+    space_grid (list), contains position values xi of the spatial grid. Returns the values of the Gaussian 
+    wave packet at time t = 0 at all spatial grid positions as a list.'''
+    
+    # Unpacking input wave parameters from wparam arugment
+    sig_0 = wparam[0]     #
+    x0 = wparam[1]        # initial particle localization
+    k0 = wparam[2]        #
+
+    # Setting input spatial grid equal to x for to condense code
+    x = space_grid
+
+    # Calculating constant for determining initial Gaussian wave packet form
+    const_psix0 = 1/(sig_0*(np.pi)**(1/2.))**(1/2.)
+
+    # Initializing psi(x,0) array as list full of zeros of the same size as the input spatial 
+    # grid array x
+    psi_x0 = np.zeros(np.size(x))
+
+    # Calculating the initial condition values of psi for all values of x at time t = 0
+    psi_x0 = const_psix0*(np.exp(_imag_i*k0*x)*np.exp((-1*(x-x0)**2)/(2*(sig_0**2))))
+
+    return psi_x0
+
+#------------------------------------------------------------------------------------------------------------#
 # Project 4 - Main
 
 # Initial parameter declarations (using values from Lab 11 to test functionality for now)
 nspace_tbd = 300         # number of spatial grid points (int)
 ntime_tbd = 501         # number of time steps to be solved over (int)
 tau_tbd = 0.016666666666666666 # tau is the time step to be solved with (float)
+
+# Global constants (identified by _ as first character)
+_plk_given = 1  # plancks constant
+_m_given = 0.5  # particle mass (m)
+_imag_i = 1j    # complex number i (root(-1))
 
 # Function that solves the one-dimensional, time-dependent Schroedinger Equation
 def sch_eqn(nspace, ntime, tau, method='ftcs', length=200, potential=[], wparam=[10,0,0.5]):
@@ -67,10 +124,6 @@ def sch_eqn(nspace, ntime, tau, method='ftcs', length=200, potential=[], wparam=
     h = L/(nspace-1)         # x_grid spacing parameter (minus 1 so it goes perfectly from -L/2 to L/2)
     method = method.lower()  # modifying input string to be all lowercase for versatility
 
-    # Constants
-    _plk_given = 1  # plancks constant
-    _m_given = 0.5  # particle mass
-
     # Initializing the x grid array to solve over (tested - appears to be working properly)
     x_grid = np.arange(-L/2,L/2+h,h)
 
@@ -78,11 +131,12 @@ def sch_eqn(nspace, ntime, tau, method='ftcs', length=200, potential=[], wparam=
     t_grid = np.arange(0,ntime*tau,tau)
 
     # Initializing the potential array (tested - appears to be working properly)
-    Vx_initial = np.empty(np.size(x_grid))  # Creating empty array of potentials to match each x position
-    Vx_initial[potential] = 1               # Setting V = 1 for all values specified by input arg potential
+    Vx_initial = np.empty(np.size(x_grid))  # creating empty array of potentials to match each x position
+    Vx_initial[potential] = 1               # setting V = 1 for all values specified by input arg potential
 
     # Evolution matrices for FTCS and Crank-Nicolson integration schemes
-    # NEXT STEP
+    # NEED TO ADAPT OLD CODE FIRST
+
 
     # Logic pathway for FTCS integration (default)
     if method == 'ftcs':
@@ -99,7 +153,7 @@ def sch_eqn(nspace, ntime, tau, method='ftcs', length=200, potential=[], wparam=
             print('PROCEEDING WITH INTEGRATION')
     
             # Then integrate with FTCS scheme
-            return #solution to sch eqn
+            return #solution to sch eqn, total probability at each timestep
         
         # If unstable, notify user and terminate integration
         else:
